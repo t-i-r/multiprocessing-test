af583cf3fa360bcf56ab0998500b23c6a24dfb14
t-i-r <peppa987@gmail.com>
2020-09-15 21:02:45 -0400

    Fixed test to pass flake8

--- tests/test_df.py
+++ tests/test_df.py
@@ -1,6 +1,5 @@
-import pytest
-import pandas as pd 
-
+# import pytest
+import pandas as pd
 
 
 def test_df_diff(df_regression):
@@ -8,5 +7,9 @@ def test_df_diff(df_regression):
     df1 = pd.DataFrame({"a": [1, 2], "b": [3, 5.0]})
     df2 = pd.DataFrame({"a": [1, 2], "b": [3, 4.0]})
 
-    # pick on df1 or df2 
+    print(df1)
+    # pick on df1 or df2
     df_regression.check(df2)
+
+    # another test
+    # assert df_regression.check(df2)
